5169cdc63a45ff075ac6cb01c98d8ad787e95b66
Ngô Văn Tuấn <tuannvd00538@fpt.edu.vn>
2018-03-19 23:45:20 +0700

    :|

--- T2TEAM_TuanNV/Redirect/script.js
+++ T2TEAM_TuanNV/Redirect/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
-	$.get("https://raw.githubusercontent.com/Tuannvd00538/T2TEAM-Community/master/T2TEAM_TuanNV/link.txt", function(response) {
-		var link = response.split('\n');
+	$.get("https://raw.githubusercontent.com/Tuannvd00538/T2TEAM-Community/master/T2TEAM_TuanNV/linkAG.txt", function(response) {
+		var link = response.split(';');
 		console.log(link.length);
 		$('#results').html('<img src="' + link[Math.floor((Math.random() * 100) + 1)] + '"/>')
 	});
